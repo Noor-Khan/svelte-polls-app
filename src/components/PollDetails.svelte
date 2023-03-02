4c8f29739ad68db4f81eaeb92a0150af36c3b7c9
noor-khan <foruconnect@gmail.com>
2023-03-02 15:36:35 +0500

    create cast vote func and vote bar

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -1,22 +1,34 @@
 <script>
   import Card from "../global/Card.svelte";
+  import { createEventDispatcher } from "svelte";
+  let dispatch = createEventDispatcher()
   export let poll;
-  let totalPolls
+  let totalPolls;
+  let percentA;
+  let percentB;
   $: totalPolls = poll.votesA + poll.votesB;
+  $: percentA = poll.votesA / totalPolls * 100
+  $: percentB = poll.votesB / totalPolls * 100
+
+  let upvote = (option, id) => {
+    dispatch('upvote', {option, id})
+  }
 </script>
 <Card>
   <div>
     <h3>{poll.question}</h3>
     <div class="total">Total Polls: {totalPolls}</div>
     <p class="total-polls"></p>
-    <div class="answer">
-      <div class="percent percent-a"></div>
+    <div class="answer" on:click={()=> upvote('a', poll.id)}>
+      <div class="percent percent-a" style="width: {percentA}%; background: {percentA < percentB ? '#ddd' : '#b5b5b5'}"></div>
       <span>{poll.answerA} ({poll.votesA})</span>
     </div>
-    <div class="answer">
-      <div class="percent percent-b"></div>
+    <i>{Math.round(percentA)}%</i>
+    <div class="answer" on:click={()=> upvote('b', poll.id)}>
+      <div class="percent percent-b" style="width: {percentB}%; background: {percentB < percentA ? '#ddd' : '#b5b5b5'}"></div>
       <span>{poll.answerB} ({poll.votesB})</span>
     </div>
+    <i>{Math.round(percentB)}%</i>
   </div>
 </Card>
 
@@ -27,18 +39,26 @@ h3 {
   font-size: 16px;
 }
 .answer {
-  border: 1px solid snow;
   box-shadow: 1px 1px 2px #eee;
-  margin: 10px 0;
+  margin-top: 20px;
   cursor: pointer;
+  position: relative;
 }
 .answer:hover {
   opacity: 0.6;
 }
+.percent {
+  position: absolute;
+  height: 100%;
+}
 span {
   display: inline-block;
+  position: relative;
   padding: 15px 10px;
   font-size: 14px;
 
+}
+i {
+  font-size: 12px;
 }
 </style>
